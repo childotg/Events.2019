2d57e069f4821a0a759ce6bda63420f075243a9d
Roberto Freato <child@ocdstudio.net>
2019-04-01 19:05:40 +0200

    DB model, migrations, data preparation & import

---
+++ 190406 - CodeCamp @Iasi/CodeCamp2019/Dryrun/CodeCamp2019.DBCreator/AppsContext.cs
@@ -0,0 +1,28 @@
+﻿using CodeCamp2019.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCamp2019.DBCreator
+{
+    public class AppsContext:DbContext
+    {
+        private readonly string _connectionString = null;
+        public AppsContext(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public AppsContext():this("")
+        {            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public DbSet<AppItem> Applications { get; set; }
+    }
+}
---
+++ 190406 - CodeCamp @Iasi/CodeCamp2019/Dryrun/CodeCamp2019.DBCreator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeCamp2019.DBCreator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new DBCreatorTask(
+                new LoggerFactory().AddConsole(),
+                args[0],args[1]).RunAsync();
+        }
+    }
+}
