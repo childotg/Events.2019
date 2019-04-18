54d0c0309d35ffd98dc3a7236bc59a5d11233fb1
Roberto Freato <child@ocdstudio.net>
2019-04-18 23:07:58 +0200

    GABTorino release

---
+++ 190427 - Global Azure Bootcamp @Torino/APIGateway/GABTorino/SalesOfAggregator.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GABTorino
+{
+    public class SalesOfAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamResponse> responses)
+        {
+            var c = JsonConvert.DeserializeObject<CustomerModel>(await responses[0].Content.ReadAsStringAsync());
+            var s = JsonConvert.DeserializeObject<SalesModel[]>(await responses[1].Content.ReadAsStringAsync());
+            return new DownstreamResponse(new HttpResponseMessage()
+            {
+                Content=new StringContent(JsonConvert.SerializeObject(new
+                {
+                    CustomerID=c.customerID,
+                    CustomerName=$"{c.firstName} {c.lastName}",
+                    CustomerSales=s.Sum(p=>p.subTotal)
+                }))
+            });
+        }
+    }
+
+    public class CustomerModel
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public class SalesModel
+    {
+        public double subTotal { get; set; }
+    }
+}
