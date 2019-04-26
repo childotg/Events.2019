c7dfbab20eefa8f6c16d87538f9e0c5c8e905071
Roberto Freato <child@ocdstudio.net>
2019-04-27 00:14:32 +0200

    gab milano finalized

---
+++ 190427 - Global Azure Bootcamp @Milano/PaaSAutomation/CMakersDryRun/Models.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMakersDryRun
+{
+    public static class S
+    {
+        public static string SqlServer = "cmakers";
+        public static string SqlUser = "cmakersAdmin";
+        public static string SqlPassword = "";
+        public static string SqlPool = "CMakersBasic";
+        public static string ResourceGroup = "CMakers";
+        public static string AppServicePlanRG = "Ocelot";
+        public static string AppServicePlan = "DemosWin";
+        public static string DbImage = "https://cmakers.blob.core.windows.net/images/dbImage.bacpac";
+        public static string StorageKey = "";
+        public static string DbConnectionKey = "Database:Connection";
+        public static string DbConnectionValue = "Server=tcp:cmakers.database.windows.net,1433;Initial Catalog=t-{0};Persist Security Info=False;User ID={1};Password={2};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public static string TenantKey = "TenantID";
+        public static string CustomDomain = "demo.cloudmakers.xyz";
+        public static string WebAppImage = "https://cmakers.blob.core.windows.net/images/webApp.zip?st=2019-04-26T20%3A48%3A43Z&se=2020-04-27T20%3A48%3A00Z&sp=r&sv=2018-03-28&sr=b&sig=2%2BymnYEA8eGfY4lRTpBRjmvIQW5qOSJbosFgc9fPz9c%3D";
+        public static int DNSTtl = 60;
+        public static string EventGridTopicEndpoint = "https://cmakers.westeurope-1.eventgrid.azure.net/api/events";
+        public static string EventGridTopicKey = "";
+        public static string ServiceBus = "Endpoint=sb://cmakers.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=";
+        public static string ServiceBusTopic = "acquisition";
+        public static string ServiceBusSubscription = "AzureProvisioner";
+        public static string AzureClientID = "25e992fd-bdcf-4c52-ba29-bdef95c507ac";
+        public static string AzureClientSecret = "";
+        public static string AzureTenantId = "";
+        public static string AzureSubscription = "";
+
+
+    }
+
+    public class Rootobject
+    {
+        public string RequestID { get; set; }
+        public string TenantName { get; set; }
+    }
+
+    public class DataModel
+    {
+        public string URL { get; set; }
+    }
+}
---
+++ 190427 - Global Azure Bootcamp @Milano/PaaSAutomation/CMakersDryRun/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CMakersDryRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        const string ServiceBusConnectionString = "Endpoint=sb://cmakers.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=2ipyTpDGxHTBBgSMpYy2fi/sw0Y/HTDi/MW5ov/SWRw=";
+        const string TopicName = "acquisition";
+        const string SubscriptionName = "AzureProvisioner";
+        static ISubscriptionClient subscriptionClient;
+
+        static async Task MainAsync()
+        {
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Register subscription message handler and receive messages in a loop.
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = true
+            };
+
+            // Register the function that processes messages.
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            await new Runner().RunAsync(JsonConvert.DeserializeObject<Rootobject>(Encoding.UTF8.GetString(message.Body)));
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
