41eb13ed10ed0b253423bd136ffb6a4b089f72fa
Roberto Freato <child@ocdstudio.net>
2019-05-14 09:17:54 +0200

    st. petersburg code and slides

---
+++ 190515 - DotNext @Saint Petersburg/APIGateway/DotNextDR/SalesOfAggregator.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Ocelot.Middleware;
+using Ocelot.Middleware.Multiplexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNextDR
+{
+    public class SalesOfAggregator : IDefinedAggregator
+    {
+        public async Task<DownstreamResponse> Aggregate(List<DownstreamResponse> responses)
+        {
+            var customer = JsonConvert.DeserializeObject<CustomerItem>
+                (await responses.First().Content.ReadAsStringAsync());
+            var sales = JsonConvert.DeserializeObject<SalesItem[]>
+                (await responses.Skip(1).First().Content.ReadAsStringAsync());
+            return new DownstreamResponse(new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(new
+                {
+                    CustomerID = customer.customerID,
+                    CustomerName = $"{customer.firstName} {customer.lastName}",
+                    CustomerSales = sales.Sum(p => p.subTotal)
+                },Formatting.Indented))
+            });
+        }
+    }
+
+    public class CustomerItem
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public class SalesItem
+    {
+        public double subTotal{ get; set; }
+    }
+}
---
+++ 190515 - DotNext @Saint Petersburg/APIGateway/DotNextDR/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace DotNextDR
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration=null;
+        public Startup()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("ocelot.json", false, true)
+                .Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(_configuration)
+                .WithConfigurationRepository()
+                .AddSingletonDefinedAggregator<SalesOfAggregator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
