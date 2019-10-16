db7fb06b998dc499366928b4906c53f6b7613175
childotg <child@ocdstudio.net>
2019-10-16 11:52:26 +0200

    Appinfo app draft

---
+++ 191025 - NETFest @Kyiv/AppService/AppServiceDeepDive/InfoApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace InfoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+
+        public Dictionary<string,string> Variables { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Variables = Environment.GetEnvironmentVariables().Keys.OfType<string>()
+                .Select(p => new { Key = p, Value = Environment.GetEnvironmentVariable(p) })
+                .ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+}
