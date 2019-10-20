74d94218a0ceb2624abda19e41981fe9d09f9894
childotg <child@ocdstudio.net>
2019-10-20 10:54:30 +0200

    asdd governance

---
+++ 191025 - NETFest @Kyiv/AppService/InfoApp/BackgroundWork.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InfoApp
+{
+    public class BackgroundWork : BackgroundService
+    {
+        private readonly ILogger _logger;
+        public BackgroundWork(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<BackgroundWork>();
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Hello from {Environment.MachineName} at {DateTime.UtcNow}");
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
--- 191025 - NETFest @Kyiv/AppService/InfoApp/Startup.cs
+++ 191025 - NETFest @Kyiv/AppService/InfoApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace InfoApp
 {
@@ -23,8 +24,9 @@ namespace InfoApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IConfiguration>(Configuration);
-            services.AddRazorPages();
-
+            services.AddRazorPages();            
+            services.AddLogging(b=>b.AddConsole());            
+            services.AddHostedService<BackgroundWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,7 @@ namespace InfoApp
             {
                 app.UseExceptionHandler("/Error");
             }
+            
 
             app.UseStaticFiles();
 
