bab7d0603ac023e7201fe28b1f5ca424e2dbf753
Roberto Freato <child@ocdstudio.net>
2019-11-28 10:47:02 +0100

    Azure TFTT finals

---
+++ 191128 - Azure TFTF Reloaded @Milano/AppService/HybridAPI/Controllers/ActorsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace HybridAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActorsController : ControllerBase
+    {
+        
+
+        private readonly ILogger<ActorsController> _logger;
+
+        public ActorsController(ILogger<ActorsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ActorModel> Get()
+        {
+            var connStr = "Server=LAPTOP; Database=sakila; Uid=root; Pwd=root;";
+            using (var conn=new MySqlConnection(connStr))
+            {   
+                conn.Open();
+                var command = new MySqlCommand("SELECT * FROM sakila.actor", conn);
+                //command.CommandType = System.Data.CommandType.Text;
+                //command.CommandText = "SELECT * FROM sakila.actor";
+                var result = command.ExecuteReader();
+                var list = new List<ActorModel>();
+                while (result.Read())
+                {
+                    list.Add(new ActorModel()
+                    {
+                        Firstname = result.GetString(1),
+                        Lastname = result.GetString(2)
+                    });                    
+                }
+                return list;
+            }
+        }
+    }
+
+    public class ActorModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ 191128 - Azure TFTF Reloaded @Milano/AppService/InfoApp/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace InfoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+        public Dictionary<string,string> Variables { get; set; }
+        public IndexModel(ILogger<IndexModel> logger,IConfiguration config)
+        {
+            _logger = logger;
+            Message = config["MySetting"];
+        }
+
+        public void OnGet()
+        {
+            Variables = Environment.GetEnvironmentVariables().Keys.OfType<string>()
+                .Select(p => new { Key = p, Value = Environment.GetEnvironmentVariable(p) })
+                .ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+}
